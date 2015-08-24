7d990ab574346a7ff4462978f87e5cb76ad69579
unknown <sebner@AUCLPSLE188.us.planview.world>
2015-08-24 13:01:34 -0500

    Set maximum page width to 1000px.

--- de.js
+++ de.js
@@ -8,7 +8,7 @@ Hubs.onPageChange = function() {
 
 $( ".large-header" ).html( $( "#planview-header-wrapper" ).html() ).removeClass( "large-header" ).css( { "display" : "block", "background" : "none" } );
 $( "#planview-header-wrapper" ).css( "display", "none");
-$('#moveToTop a').prop('title', 'Back to Top ↑');
+$('#moveToTop a').prop('title', 'Zurück nach oben ↑');
 
 var secondaryNav = $('.top-nav'),
 	secondaryNavTopPosition = secondaryNav.offset().top;
@@ -41,37 +41,50 @@ $('button#planview-nav-mobil-toggle-button').click(function() {
   }
 });
 
-$(window).resize(function(){
+$(window).on('resize', function(e){	
 	if ($(window).width() >= 992 && $('ul.planview-nav-mobil').hasClass('open')) {
 		$('ul.planview-nav-mobil').slideUp("slow");
 		$('ul.planview-nav-mobil').removeClass('open');
 	}
-	//resizeLimelightPlayer('resize');
-	
-	//max-width: 1100px;
-	if ($( ".page-width" ).width() >= 1100) {
-		$( ".page-width" ).width('1100px');
-		$( ".page-aligner" ).width('1100px');		
-		setLeftMargin();
-	}
+	setPageAlignerClass();
+	resizeLimelightPlayer('resize');
 });
 
-function setLeftMargin() {
-	console.log('window width = ' + $(window).width() + '\npage-width class width = ' + $( ".page-width" ).width());
-	//if ($( ".page-width" ).width() >= 1200) {
-	//if ($( ".page-width" ).width() == 1100) {
-		var leftMarginValue = (($(window).width() - $( ".page-width" ).width()) / 2);
-		//var leftMarginValue = $(window).width();// - 1100;
-		//leftMarginValue = leftMarginValue / 2;
-		$( ".page-width" ).css( "margin-left", leftMarginValue);
-		console.log('window width = ' + $(window).width() + '\npage-width class width = ' + $( ".page-width" ).width() + '\nmargin-left = ' + leftMarginValue);
-		//console.log('margin-left = ');
-		//console.log(leftMarginValue);
-	//}
+function setPageAlignerClass() { 
+	if ($(window).outerWidth() <= 543) { //Uberflip change point is 560
+		$( ".page-width .page-aligner" ).css( "width", '96%');
+	} else if ($(window).outerWidth() <= 562) { //Uberflip change point is 560
+		$( ".page-width .page-aligner" ).css( "width", 540).css( "margin-left", 0);
+	} else if ($(window).outerWidth() <= 720) { //Uberflip change point
+		$( ".page-width .page-aligner" ).css( "width", 540).css( "margin-left", "auto");
+	} else if ($(window).outerWidth() <= 845) { //Uberflip change point is 860
+		$( ".page-width .page-aligner" ).css( "width", 580);
+	} else if ($(window).outerWidth() <= 1336) { //Uberflip change point
+		$( ".page-width .page-aligner" ).css( "width", 810);
+	} else {
+		$( ".page-width .page-aligner" ).css( "width", 1080);
+	}
+	// set left margin for 'back to top' link
+	var moveToTopMarginLeft = 0;
+	// does not show on mobile
+	if ($(window).outerWidth() <= 720) { //Uberflip change point
+		$( "#moveToTop a" ).css( "display", "none");
+	} else { 
+		$( "#moveToTop a" ).css( "display", "block");
+		if ($(window).outerWidth() <= 845) { //Uberflip change point
+			moveToTopMarginLeft = ((($(".page-width .page-aligner").outerWidth() / 2) + 50) * -1);
+		} else if ($(window).outerWidth() <= 965) { //Uberflip change point is 980
+			moveToTopMarginLeft = 0;
+		} else {
+			moveToTopMarginLeft = ((($(".page-width .page-aligner").outerWidth() / 2) + 50) * -1);
+		}
+	}
+	//console.log('window.outerWidth = ' + $(window).outerWidth() +'\nmoveToTopMarginLeft = ' + moveToTopMarginLeft);
+	$( "#moveToTop" ).css( "margin-left", moveToTopMarginLeft);
 }
 
 function resizeLimelightPlayer(msg) {
-	//alert('resizeLimelightPlayer - ' + msg);
+	//console.log('resizeLimelightPlayer - ' + msg);
 	if ( $('.limelight-video-respond').length ) { //if Limelight player exists
 		$('.limelight-video-respond').each(function () {
 			var $wrapper = $(this),
@@ -96,6 +109,8 @@ function resizeLimelightPlayer(msg) {
 	}
 }
 
+var setPageAlignerClassOnLoad = setPageAlignerClass();
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -104,5 +119,3 @@ ga('create', 'UA-16646450-1', 'auto', {'name': 'newTracker', 'allowLinker': true
 ga('newTracker.require', 'linker'); 
 ga('newTracker.linker:autoLink', ['go.planview.com', 'resources.planview.com', 'www.planview.com', 'www.planview.de', 'www.planview.fr'] ); 
 ga('newTracker.send', 'pageview'); 
-
-var setLeftMarginOnLoad = setLeftMargin();
